31f49bf0ba8c39d2b02f28a214a724f55fcc1871
William Chia <william.y.chia@gmail.com>
2017-04-15 19:16:28 -0500

    running matter.js

--- Wubba/file.js
+++ Wubba/file.js
@@ -1,9 +1,23 @@
+var source, fft;
+
+function setup() {
+  source = new p5.AudioIn();
+  source.start();
+
+  fft = new p5.FFT(0.8, 1024);
+  fft.setInput(source);
+}
+
 function start() {
 	// module aliases
 	var Engine = Matter.Engine,
 		Render = Matter.Render,
 		World = Matter.World,
 		Bodies = Matter.Bodies;
+		Body = Matter.Body;
+		Composites = Matter.Composites;
+		Events = Matter.Events;
+		Runner = Matter.Runner;
 
 	// create an engine
 	var engine = Engine.create();
@@ -11,27 +25,66 @@ function start() {
 	// create a renderer
 	var render = Render.create({
 		element: document.body,
-		engine: engine
+		engine: engine,
+		options: {
+			width: 1280,
+			height: 900,
+			wireframes: false
+		}
 	});
 
-	// create two boxes and a ground
-	var boxA = Bodies.circle(400, 200, 80);
-	var boxB = Bodies.circle(450, 50, 80);
-	var ground = Bodies.rectangle(400, 300, 810, 60, { isStatic: true});
+	var group = Body.nextGroup(true);
+	
+	var waves = Composites.stack(20, 300, 41, 1, 2, 2, function(x, y) {
+		return Bodies.rectangle(x, y, 20, 10, {collisionFilter: {group: group}});
+	});
+	
+	var door = Bodies.rectangle(20+20*40, 240, 24, 60);
+	door.isSensor = true;
+	door.isStatic = true;
+	
+	Composites.chain(waves, 0.5, 0, -0.5, 0, { stiffness: 0.9 });
+	
+	var ball = Bodies.circle(400, 20, 20, {
+		render: {
+			strokeStyle: "#ff0000",
+			fillStyle: "#bb0000",
+			lineWidth: 11
+		}
+	});
+	
+	var block = Bodies.rectangle(440, 455, 1000, 300, {
+		isStatic: true
+	});
+	
+	ball.friction = 0;
+	Matter.Body.setMass(ball, 0.5);
 
 	// add all of the bodies to the world
-	World.add(engine.world, [boxA, boxB, ground]);
+	World.add(engine.world, [waves, ball, block]);
 
 	// run the engine
 	Engine.run(engine);
 
 	// run the renderer
 	Render.run(render);
+	/*
+	Events.on(engine, "collisionStart", function(event) {
+		alert("woo");
+	});
+	*/
 	
-	var dump = 1;
+	waves.bodies[0].isStatic = true;
+	waves.bodies[40].isStatic = true;
+	var deriv = [];
+	
 	setInterval(function(){
-		Matter.Body.setVelocity(ground, Matter.Vector.create(0, 5*Math.sin(dump)));
-		dump+=0.1;
+		var spectrum = fft.analyze();
+		
+		for (var a = 1; a < waves.bodies.length-1; a++) {
+			let val = spectrum[4+a]/4;
+			Matter.Body.setVelocity(waves.bodies[a], Matter.Vector.create(0,deriv[a] - val));
+			deriv[a] = val;
+		}
 	}, 10);
-	
 }
--- Wubba/sketch2.js
+++ Wubba/sketch2.js
@@ -12,14 +12,23 @@ function init() {
 	var canvas_thing = document.getElementById("canvo");
 	var canvas = canvas_thing.getContext("2d");
 	canvas.fillStyle = "#ff0000";
+	var highest_point = 0;
+	var highest_peak = 0;
 	
 	setInterval( function(){
 		canvas.clearRect(0, 0, canvas_thing.width, canvas_thing.height);
 		source = fft.analyze();
 		
+		//Human singing range is between values 4 and 45
+		
 		//draw really small rectangles along the canvas
-		for (var a = 0; a < source.length; a++) {
+		for (var a = 4; a < 45; a++) {
 			canvas.fillRect(2*a, 600-source[a], 2, source[a]);
+			if (source[a] > highest_peak) {
+				highest_peak = source[a];
+				highest_point = a;
+			}
 		}
+		canvas.fillText("Highest point at [" + highest_point + "], " + highest_peak, 10, 10);
 	}, 30);
 }
